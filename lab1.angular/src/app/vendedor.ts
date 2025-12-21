be8847ea695778ff2fc9b5bb0113c977671a6cb5
Mateo Medranda <matemedranda15@gmail.com>
2025-12-21 16:04:55 -0500

    Agregando pruebas de vendedor angular

---
+++ lab1.angular/src/app/vendedor.ts
@@ -0,0 +1,16 @@
+export class Vendedor {
+    id?: number;
+    name: string;
+    email: string;
+    telefono: string;
+    comision: number;
+    codigoEmpleado: string;
+
+    constructor(name: string, email: string, telefono: string, comision: number, codigoEmpleado: string) {
+        this.name = name;
+        this.email = email;
+        this.telefono = telefono;
+        this.comision = comision;
+        this.codigoEmpleado = codigoEmpleado;
+    }
+}
