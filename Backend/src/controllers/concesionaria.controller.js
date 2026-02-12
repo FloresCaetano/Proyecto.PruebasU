bd7578dab447f95f1f2c73792939cf17dff614c5
Caetano Flores <caetano.flores@hotmail.com>
2026-02-12 08:48:15 -0500

    Fix: Add missing addNewConcesionaria export and add id field to all controllers

--- Backend/src/controllers/concesionaria.controller.js
+++ Backend/src/controllers/concesionaria.controller.js
@@ -131,6 +131,7 @@ async function _clearConcesionarias() {
 module.exports = { 
     getAllConcesionarias,
     getConcesionariaById, 
+    addNewConcesionaria,
     updateConcesionaria, 
     deleteConcesionaria,
     _clearConcesionarias 
